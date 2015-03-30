f66ad4d3d52de5cff66afd31050b45a9e48d9f7d
Nick Stewart <nick.stewart@rakuten.com>
2015-03-30 09:01:23 -0600

    Fixed build and adds a couple tests for the Ball Clock.

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ lazy val root = (project in file(".")).
     settings(
         name := "scala-rolling-ball-clock",
         version := "1.0",
-        scalaVersion := "2.11.6"
+        scalaVersion := "2.11.6",
+        libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
     )
-    libraryDependencies += "org.scalatest" % "scalatest" % "2.2.4" % "test"
--- src/test/scala/BallClockTests.scala
+++ src/test/scala/BallClockTests.scala
@@ -1,8 +1,15 @@
-package test.scala
+import org.scalatest._
 
 /**
  * Created by nickstewart on 3/27/15.
  */
-class BallClockTests {
-    
+class BallClockTests extends FlatSpec with Matchers {
+    "A Ball Clock" should "find the answer for 30 balls" in {
+        val clock = BallClock(30)
+        clock.run() should be (15)
+    }
+    it should "find the answer for 45 balls" in {
+        val clock = BallClock(45)
+        clock.run() should be (378)
+    }
 }
